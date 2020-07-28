eeb0299b49cd88387781859f2b0455daba2243f8
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2020-07-28 23:30:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,51 +1,53 @@
-var car, wall, speed, weight;
+var car,wall;
 
+
+var speed, weight; 
 
 
 function setup() {
-  createCanvas(800,400);
- speed = random(55,90);
- weight = random(500,1600);
+  createCanvas(1600, 400);
 
- 
-  car = createSprite(50, 200, 50, 50);
-  wall = createSprite(650,200,60,200);
-  car.shapeColor = "black";
-  wall.shapeColor = "black";
+
+	speed=random(55,90)
+	weight=random(400,1500)
+
+
+	car=createSprite(50, 200, 50,50);   
+
+	car.velocityX = speed;
+
+	car.shapeColor=color(255);
+
+
+  
+  	wall=createSprite(1500,200, 60, height/2)
+  	wall.shapeColor=color(80,80,80)
 }
+
 
 function draw() {
-  background("white");  
+  background(0);
+
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22509;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
   
-  wall.debug = true
-  car.debug = true
-  if(keyDown("space"))
-{
-  car.x=50;
-  car.y=200;
-  car.velocityX=speed;
-}
-if(wall.x -car.x<(car.width-wall.width)/2);
-{
-  car.velocityY=8;
-
-car.y = 200
-car.velocityX=speed;
-}
-if(wall.x - (car.x<car.width-wall.width)/2)
-
-  var deformation = 0.5 = weight = speed = speed / 22500;
-  if(deformation>180)
-{
-  car.velocityX=0;car.shapeColor = color(255,0,0)
-}  
-if(deformaation <180 && deformation > 100) 
-{
-  car.shapeColor = color(230,230,0)
-}
-if(deformation < 100)
-{
-  car.shapeColor=color(0,255,0);
-}
   drawSprites();
+ 
 }
